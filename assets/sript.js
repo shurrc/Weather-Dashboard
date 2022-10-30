cda22e6ec2afd34da4351f05145afc05b6205aff
Kathy Shurr <kathyshurr@kathys-mbp.myfiosgateway.com>
2022-10-30 19:32:11 -0400

    Built js, added dynamically generated elements, configured HTML

--- assets/sript.js
+++ assets/sript.js
@@ -1,7 +1,12 @@
 var apiKey = "81cbf1ebdb55fbe936d5592aa68a8afa";
-// var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 var searchButton = document.getElementById("search-button");
 var searchInput = document.getElementById("search-input");
+var weatherContainer = document.getElementById("weather-display");
+var currentWeather = document.getElementById("current-weather");
+var futureWeather = document.getElementById("future-weather");
+var currentCard = document.getElementById("current-card");
+var currentDate = moment().format("dddd");
+
 
 function locationSearch(city) {
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
@@ -16,31 +21,73 @@ function locationSearch(city) {
         getCurrentWeather(lat, long);
         getFutureWeather(lat, long);
     })
-    
 }
-//use name taken from location data
-//keep it modular, seperate functions for rendering current/future weather displays and saving to local storage
-
 function getCurrentWeather(lat, long) {
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`
     fetch(queryURL)
     .then(function(data){
         return data.json()
     })
     .then(function(data) {
-        console.log("current weather ",data);
+        document.querySelector("#today").textContent = "Today's Forecast"
+        document.querySelector("#city-name").textContent = data.name;
+        document.querySelector("#cTemp").textContent = "Temp: " + data.main.temp + "Fº";
+        var iconCImage = document.createElement("img");
+        var icon = data.weather[0].icon;
+        iconCImage.setAttribute(
+        "src",
+        "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+        );
+        currentCard.append(iconCImage);
+        document.querySelector("#cWind").textContent = "Wind Speed: " + data.wind.speed + "MPH";
+        document.querySelector("#cHumidity").textContent = "Humidity: " + data.main.humidity + "%";
+
     })
 }
-//increment by 8 due to getting a forecast for 3 hours
-
 function getFutureWeather(lat, long) {
-    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`
     fetch(queryURL)
     .then(function(data){
         return data.json()
     })
     .then(function(data) {
         console.log("Future weather ",data);
+        for (var i =0; i< 40; i+=8) {
+            var column = document.createElement("div");
+            column.setAttribute("class", "column");
+            weatherContainer.append(column);
+
+            var card = document.createElement("div");
+            card.setAttribute("class", "card pl-1");
+            column.append(card);
+
+            var fDate = document.createElement("h1");
+            fDate.textContent = data.list[i].dt_txt;
+            card.append(fDate)
+            console.log(data.list[0].dt_txt)
+
+            var fTemp = document.createElement("h1");
+            fTemp.textContent = "Temp: " + data.list[i].main.temp + "Fº";
+            card.append(fTemp)
+
+            var fWind = document.createElement("p");
+            fWind.textContent = "Wind Speed: " + data.list[i].wind.speed + "MPH";
+            card.append(fWind);
+            console.log(data.list[i].wind.speed);
+
+            var fHumidity = document.createElement("p");
+            fHumidity.textContent = "Humidity: " + data.list[i].main.humidity + "%";
+            card.append(fHumidity);
+
+            var iconImage = document.createElement("img");
+            var icon = data.list[i].weather[0].icon;
+            iconImage.setAttribute(
+            "src",
+            "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+            );
+            card.append(iconImage);
+
+        }
     })
 }
 
@@ -50,5 +97,11 @@ searchButton.addEventListener("click", function() {
         return;
     }
     locationSearch(searchedCity);
+
+
 })
 
+///WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
